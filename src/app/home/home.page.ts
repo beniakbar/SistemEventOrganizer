653c4a4440915616bdad0b92a62b24dd0baabc75
Beni Akbar <beniakbar555@gmail.com>
2025-04-25 10:00:10 +0700

    test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  message = '';
+
+  constructor(private api: ApiService) {}
+
+  ionViewWillEnter() {
+    this.api.getHello().subscribe((res: any) => {
+      this.message = res.message;
+    });
+  }
 
 }
